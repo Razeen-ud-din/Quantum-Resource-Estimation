7e98f7e02eba6992e6b43662309938e056561d2b
Danial <76565771+DanialHaseeb@users.noreply.github.com>
2024-08-30 18:56:04 +0500

    Refactored

--- notebooks/intro/lib/__init__.py
+++ notebooks/intro/lib/__init__.py
@@ -1,15 +1,20 @@
 from typing import Callable, Any
 
-def time(function: Callable[..., Any], runs: int = 20) -> float:
+def time(function: Callable[..., Any], runs: int = 100) -> float:
 	"""
 	Measure the time taken to execute a function.
 
-	Args:
-		function: The function to measure.
-		runs: The number of times to run the function
+	Parameters
+	----------
+		function
+			The function to measure.
+		runs
+			The number of times to run the function
 
-	Returns:
-		The time taken to execute the function.
+	Returns
+	-------
+		float
+			The time taken to execute the function.
 	"""
 	from timeit import default_timer as timer
 
@@ -23,3 +28,39 @@ def time(function: Callable[..., Any], runs: int = 20) -> float:
 		sum += elapsed_time
 
 	return sum / runs
+
+def sci(number: str) -> str:
+	"""
+	Converts a number to scientific notation.
+
+	Parameters
+	----------
+		number
+			The number to convert.
+
+	Returns
+	-------
+		str
+			The number in scientific notation.
+	"""
+
+	superscript = {
+		"0": "⁰",
+		"1": "¹",
+		"2": "²",
+		"3": "³",
+		"4": "⁴",
+		"5": "⁵",
+		"6": "⁶",
+		"7": "⁷",
+		"8": "⁸",
+		"9": "⁹",
+		"-": "⁻",
+	}
+
+	[mantissa, exponent] = number.split("e")
+	exponent = str(int(exponent))
+
+	new_exponent = "".join([superscript[symbol] for symbol in exponent])
+
+	return f"{mantissa}·10{new_exponent}"
